3f933722a59a4aa5912713c5a120f4473a8957b7
AryaBagusPutraPratama <bagusaria14@gmail.com>
2024-04-24 08:46:50 +0700

    Route update

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,31 @@ Route::get('/RW/dataUsahaRW', [UmkmController::class, 'indexDataRW'])->name('dat
 Route::get('/Penduduk/izinUsahaPenduduk', [UmkmController::class, 'indexIzinPenduduk'])->name('izinUsahaPenduduk');
 Route::get('/Penduduk/dataUsahaPenduduk', [UmkmController::class, 'indexDataPenduduk'])->name( 'dataUsahaPenduduk' );
 Route::get('/Penduduk/detailIzinUsaha', [UmkmController::class, 'indexDetailIzinPenduduk'])->name( 'detailIzinUsaha' );
+// Data Kk
+Route::get('/kk', [DataKkController::class, 'index'])->name('kk.index');
+Route::get('/kk/create', [DataKkController::class, 'create'])->name('kk.create');
+Route::post('/kk', [DataKkController::class, 'store'])->name('kk.store');
+Route::get('/kk/{id}', [DataKkController::class, 'show'])->name('kk.show');
+Route::get('/kk/{id}/edit', [DataKkController::class, 'edit'])->name('kk.edit');
+Route::put('/kk/{id}', [DataKkController::class, 'update'])->name('kk.update');
+Route::delete('/kk/{id}', [DataKkController::class, 'destroy'])->name('kk.destroy');
+// Data Penduduk
+Route::get('/penduduk', [PendudukController::class, 'index'])->name('penduduk.index');
+Route::get('/penduduk/create', [PendudukController::class, 'create'])->name('penduduk.create');
+Route::post('/penduduk', [PendudukController::class, 'store'])->name('penduduk.store');
+Route::get('/penduduk/{penduduk}', [PendudukController::class, 'show'])->name('penduduk.show');
+Route::get('/penduduk/{penduduk}/edit', [PendudukController::class, 'edit'])->name('penduduk.edit');
+Route::put('/penduduk/{penduduk}', [PendudukController::class, 'update'])->name('penduduk.update');
+Route::delete('/penduduk/{penduduk}', [PendudukController::class, 'destroy'])->name('penduduk.destroy');
+
+Route::group(['prefix' => 'usulan'], function(){
+    Route::get('/RT/usulanKegiatanRT', [kegiatanController::class,'indexRT']);
+});
+
+
+Route::group(['prefix' => 'usulan'], function(){
+    Route::get('/RW/usulanKegiatanRW', [kegiatanController::class,'indexRW']);
+});
+
+Route::group(['prefix' => 'usulan'], function(){
+    Route::get('/Penduduk/usulanKegiatanPD', [kegiatanController::class,'indexPenduduk']);
