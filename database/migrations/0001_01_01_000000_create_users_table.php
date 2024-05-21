0bf23f4fe459be374ca33a9cdce9a531adc244fd
AryaBagusPutraPratama <bagusaria14@gmail.com>
2024-05-21 10:42:15 +0700

    menambahkan kolom rt pada tabel user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->unsignedBigInteger('role_id');
+            $table->integer('rt');
             $table->rememberToken();
             $table->timestamps();
 
