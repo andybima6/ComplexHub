d4d97f449de6aed0b20f6e57a2396c75874bf5d4
Andy Nugraha Putra <andybima6@gmail.com>
2024-05-27 23:54:45 +0700

    perbaiikan landing page

--- database/seeders/UmkmSeeder.php
+++ database/seeders/UmkmSeeder.php
@@ -20,7 +20,7 @@ class UmkmSeeder extends Seeder
             'foto_produk' => 'foto-produk/wHiNCDGnnWZGJ11xUm70xXwhDIhlxvCZrUUg36js.png',
             'status_rt' => 'pending',
             'status_rw' => 'pending',
-            'rt_id' => 1,
+          
         ]);
         Umkm::create([
             'nama_warga' => 'lukman',
@@ -29,7 +29,7 @@ class UmkmSeeder extends Seeder
             'foto_produk' => 'foto-produk/R4KsDSVEtK0Gke7IDZfLDIIMNb2MEn1q73qdseow.png',
             'status_rt' => 'pending',
             'status_rw' => 'pending',
-            'rt_id' => 1,
+          
         ]);
         Umkm::create([
             'nama_warga' => 'lukman',
@@ -38,7 +38,7 @@ class UmkmSeeder extends Seeder
             'foto_produk' => 'foto-produk/W9ONIIptXFbqasrvBYdXyJpWXvTlNauut6SBr6EW.png',
             'status_rt' => 'pending',
             'status_rw' => 'pending',
-            'rt_id' => 1,
+          
         ]);
         Umkm::create([
             'nama_warga' => 'lukman',
@@ -47,7 +47,7 @@ class UmkmSeeder extends Seeder
             'foto_produk' => 'foto-produk/179cAiEckQzyZBFKiTL1LB9OZhaQ3d78DCIzVnT7.png',
             'status_rt' => 'pending',
             'status_rw' => 'pending',
-            'rt_id' => 1,
+          
         ]);
     }
 }
